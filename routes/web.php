31d96f427cab6d6bb983382a5fffc1bb93952707
maersyifaaa <112466612+maersyifaaa@users.noreply.github.com>
2023-11-23 07:32:54 +0700

    mengatur route

--- routes/web.php
+++ routes/web.php
@@ -17,48 +17,64 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tulis', function () {
-    return 'welcome';
-});
-
-Route::get('/sidebar', function() {
-    return view('sidebar'); //ini surat_Masuk tp blm ku rename
-});
-
-Route::get('/surat_keluar', function() {
-    return view('surat_keluar');
-});
-
-Route::get('/daftar_surat_keluar', function() {
-    return view('daftar_surat_keluar');
-});
-
-Route::get('/profile', function() {
-    return view('profil');
-});
-
-Route::get('/kontrak', function() {
-    return view('kontrak');
-});
-
-Route::get('/home', function() {
-    return view('homepage');
-});
-
 //
 Route::get('/homedump', function() {
     return view('cobahomepage');
 });
 
-Route::get('/pelaporan_hukum', function() {
-    return view('pelaporan_hukum');
+Route::get('/tulis', function () {
+    return 'welcome';
 });
 
+//route yg dipake mulai ini
 
+//route sign in - balqis
+
+//route dashboard - balqis
+Route::get('/home', function() {
+    return view('dashboard');
+});
+
+//route profile - charisa
+Route::get('/profile', function() {
+    return view('profile.profile');
+});
+
+//route edit profile - charisa
+
+//route kontrak - zahra
+Route::get('/kontrak', function() {
+    return view('kontrak');
+});
+
+//route create surat masuk - zahra
+Route::get('/surat-masuk', function() {
+    return view('surat-masuk.surat_masuk'); 
+});
+
+//route create surat keluar - salsha
+Route::get('/surat-keluar', function() {
+    return view('surat-keluar.surat_keluar');
+});
+
+//route status surat keluar - salsha
+Route::get('/status-surat-keluar', function() {
+    return view('status-surat.status_surat_keluar');
+});
+
+//route create pelaporan hukum - ida
+Route::get('/pelaporan-hukum', function() {
+    return view('pelaporan-hukum.pelaporan_hukum');
+});
+
+//route privacy policy - ida
+
+//route status pelaporan hukum - fika
+Route::get('/status-pelaporan', function() {
+    return view('status-surat.status_pelaporan_hukum');
+});
+
+//route regulasi - fika
 Route::get('/regulasi', function() {
-    return view('regulasi');
-});
-
-Route::get('/cek_status_hukum', function() {
-    return view('cek_status_pelaporan_hukum');
+    return view('regulasi.regulasi');
 });
