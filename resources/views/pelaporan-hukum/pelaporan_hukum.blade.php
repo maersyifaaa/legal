9e9b2fe2f84dfee146060eab83ead8389201af5b
firdakusuma <firdakusumaaa@gmail.com>
2023-11-28 20:47:05 +0700

    edit

--- resources/views/pelaporan-hukum/pelaporan_hukum.blade.php
+++ resources/views/pelaporan-hukum/pelaporan_hukum.blade.php
@@ -8,6 +8,8 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/datepicker.min.js"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
@@ -85,9 +87,114 @@
             </li>
         </ul>
     </div>
-</aside>
 
+    </aside>
 
+    {{-- content --}}
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-white">
+            <div class="grid grid-cols-2 gap-4 mb-4">
+                <div class="flex py-8 h-24 rounded bg-gray-50 dark:bg-clear-400 w-1/2">
+                    <p class="w-5.5 h-3.5 text-zinc-800 text-4xl font-extrabold font-['Poppins']">Pelaporan Hukum</p>
+                </div>
+
+                {{-- <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+         <p class="text-2xl text-gray-400 dark:text-gray-500">
+            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+            </svg>
+         </p>
+            </div> --}}
+            </div>
+
+            
+<form>
+    <div class="grid gap-10 mb-3 md:grid-cols-2">
+        <div>
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Nama Pelapor</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Enter Your Name" required>
+            
+            <label for="Divisi" class="block mb-1 text-sm font-medium text-gray-900 dark:text-black">NIP</label>
+            <input type="text" id="nip" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Enter Your NIP" required>
+
+            <label for="Divisi" class="block mb-1 text-sm font-medium text-gray-900 dark:text-black">Divisi</label>
+            <input type="text" id="div" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Enter Your Divisi" required>
+
+            <label class="block mb-2 py-1 text-sm font-medium text-gray-900 dark:text-black" for="file_input">Upload file</label>
+            <input class="block text-sm text-gray-900 border border-white-300 rounded-lg cursor-pointer bg-white-50 dark:text-gray-500 focus:outline-none w-3/4 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400" 
+            id="file_input" type="file">
+
+        </div>
+
+        <div>
+        <label for="perihal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Perihal</label>        
+        <button id="dropdownCheckboxButton" data-dropdown-toggle="dropdownDefaultCheckbox" class="text-black bg-amber-600 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-amber-300 block w-1/2 
+        font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-amber-600" 
+        type="button">Perihal <svg class="w-2.5 h-2.5 ms-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" >
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
+        </button>
+
+<!-- Dropdown menu -->
+<div id="dropdownDefaultCheckbox" class="z-10 hidden w-48 bg-white divide-y divide-amber-600 rounded-lg shadow dark:bg-white-600 dark:divide-amber-600">
+    <ul class="p-3 space-y-3 text-sm text-black-700 dark:text-black-500" aria-labelledby="dropdownCheckboxButton">
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-gray-100 border-blck-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-1" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Kecelakaan kerja</label>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+            <input checked id="checkbox-item-2" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-gray-100 border-black-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+            <label for="checkbox-item-2" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Kejadian Berbahaya Lain</label>
+          </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-3" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-black-100 border-blak-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-3" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Korupsi</label>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-3" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-black-100 border-black-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-3" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Kecurangan</label>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-3" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-black-100 border-black-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-3" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Penggelapan</label>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-3" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-black-100 border-black-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-3" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Suap</label>
+        </div>
+      </li>
+      <li>
+        <div class="flex items-center">
+          <input id="checkbox-item-3" type="checkbox" value="" class="w-4 h-4 text-black-600 bg-black-100 border-black-300 rounded focus:ring-black-500 dark:focus:ring-black-600 dark:ring-offset-black-700 dark:focus:ring-offset-black-700 focus:ring-2 dark:bg-black-600 dark:border-black-500">
+          <label for="checkbox-item-3" class="ms-2 text-sm font-medium text-black-900 dark:text-black-300">Pelanggaran saat pengadaan barang/jasa</label>
+        </div>
+      </li>
+    </ul>
+</div>
+
+        </div>
+        
+
+        <div>
+    <a href="/cek_status_hukum" type="submit" class="text-black bg-amber-600 focus:ring-4 focus:outline-none focus:ring-white-300 w-1/2 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 
+    text-center dark:bg-white-600 dark:hover:bg-amber-500 dark:focus:ring-black-800" >Submit
+    </a>
+        </div>
+</form>
     
 </body>
 </html>
