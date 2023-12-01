34c12ec60e44c4b8335bb39ae8d40b7224008ae4
phik753 <89512318+phik753@users.noreply.github.com>
2023-12-01 18:55:35 +0700

    iya gt deh
    zuzurly w cape sama nih sidebar dan siapa yg mau consume api dari karyawan wahai kawanku

--- resources/views/status-surat/status_pelaporan_hukum.blade.php
+++ resources/views/status-surat/status_pelaporan_hukum.blade.php
@@ -33,25 +33,25 @@
         class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-amber-600">
-            <a href="https://flowbite.com/" class="flex items-center pl-2.5 mb-5">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-7" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Legal</span>
+            <a href="/home" class="flex items-center pl-2.5 mb-5">
+                <img src="images/logo.png" class="h-10" alt="legal logo" />
+                {{-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span> --}}
             </a>
             {{-- //profil --}}
-            <div class="flex items-center justify-center my-6">
+            <div class="flex items-center justify-center my-8">
                 <div class="items-center justify-center w-full">
                     <img class="w-1/2 rounded-full mb-2 ml-14"
                         src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
                         alt="profil user">
-                    <p class="text-center">Firda Kusuma Wardani</p>
+                    <p class="text-center text-zinc-800 font-extrabold">Firda Kusuma Wardani</p>
                 </div>
             </div>
 
             <ul class="space-y-2 font-medium">
                 <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    <a href="/home"
+                        class="flex items-center p-2 my-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg class="w-5 h-5 text-black-500 transition duration-75 dark:text-black-400 group-hover:text-black-200 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 22 21">
                             <path
@@ -63,8 +63,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <a href="/kontrak"
+                        class="flex items-center p-2 my-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 18 18">
@@ -75,8 +75,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <a href="/regulasi"
+                        class="flex items-center p-2 my-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 20 20">
