1f414adeb82ccafdf8dd16f88e3cfbc0735aba19
phik753 <89512318+phik753@users.noreply.github.com>
2023-12-17 11:35:07 +0700

    Update regulasi.blade.php

--- resources/views/regulasi/regulasi.blade.php
+++ resources/views/regulasi/regulasi.blade.php
@@ -115,7 +115,8 @@
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                         data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
                         aria-controls="accordion-collapse-body-1">
-                        <span>What is Flowbite?</span>
+                        <span>Pasal 1</span>
+                        <span>Kewajiban Pegawai</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -125,13 +126,17 @@
                 </h2>
                 <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 ">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of
-                            interactive components built on top of Tailwind CSS including buttons, dropdowns, modals,
-                            navbars, and more.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a
-                                href="/docs/getting-started/introduction/"
-                                class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start
-                            developing websites even faster with components on top of Tailwind CSS.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">1. Setiap pegawai wajib datang ke tempat kerja dan masuk kerja sebelum waktu keda yang
+                            telah ditetapkan.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">2. setiap pegawai wajib melaksanakan tugas yang diberikan dengan tetap mencurahkan
+                            perhatian penuh dan selalu mengikuti petunjuk atau iostruksi dengan baik untuk
+                            memastikan bahwa produk atau layanan yang dihasilkan berkualitas.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">3.  Setiap pegawai wajib mengikuti dan mematuhi seluruh prosedur dan/atau
+                            penmjuk/instnrksi yang diberikan oleh atasannya, manajemen atau pemimpin perusahaan
+                            yang memiliki kewenangan untuk memberi petunjuk atau instruksi.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">4. Setiap pegawai wajib menunjukkan tingkah laku yang bak dan benar dengan tujuan
+                            memberikan kepuasan, kepercayaan, dan citra positif kepada pelanggan.</p>
+                       
                     </div>
                 </div>
                 <h2 id="accordion-collapse-heading-2">
@@ -139,7 +144,8 @@
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                         data-accordion-target="#accordion-collapse-body-2" aria-expanded="false"
                         aria-controls="accordion-collapse-body-2">
-                        <span>Is there a Figma file available?</span>
+                        <span>Pasal 2</span>
+                        <span>Larangan-larangan bagi Pegawai</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -149,13 +155,32 @@
                 </h2>
                 <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed
-                            using the Figma software so everything you see in the library has a design equivalent in our
-                            Figma file.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Check out the <a
-                                href="https://flowbite.com/figma/"
-                                class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based
-                            on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">1. Memberikan keteratrgan palsu mengenai riwayat hidupny4 pendidikan, dan lain-lain
+                            dalam hubungan kerja dengan perusahan. 
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">2. Membuat keputusan di luar batas wewenang sesuaiJbb description-nya.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">3. Berada di luar selain di tempat kerja di waknr/jam kerja tanpa izin dari aasan yang
+                            bersaogkutan.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">4.  Melakukan penggelapm, manipulasi, menerima uang suap, komisi karena jabatannya
+                            atau korupsi, penipuao, dan bermain judi.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">5. Menggunakan jabatan/posisi untuk kepentingan pribadi ataupun pihak lainnya seperti
+                            keluarga, teman, den lain-lain yang dapat mengganggu dan/atau merugftan kepentingan
+                            perusahaan.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">6. Menganiay4 menghina secara kasar, atau mengancam pihak pengusaha atau anggota
+                            keluarganya atau pegawai lain dan anggota keluargany4 para pelanggan (customer) dalr
+                            tamu perusahaan.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">7. MerusaVmenghilangkan buang/data-data milik perusahaan atau menempatkan baang
+                            milik perusahaan secara sembarangan dalam posisi yang mudah terbakar dan/atau
+                            terancam bahaya.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">8.  Melakukan hal-hal lain yang bertentangan dengan peraturan perusahaan dan perundangundangan yang berlaku.
+                            </p>
+                        
                     </div>
                 </div>
                 <h2 id="accordion-collapse-heading-3">
@@ -163,7 +188,8 @@
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                         data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
                         aria-controls="accordion-collapse-body-3">
-                        <span>What are the differences between Flowbite and Tailwind UI?</span>
+                        <span>Pasal 3</span>
+                        <span>Hubungan Kerja</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -173,20 +199,18 @@
                 </h2>
                 <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
                     <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core
-                            components from Flowbite are open source under the MIT license, whereas Tailwind UI is a
-                            paid product. Another difference is that Flowbite relies on smaller and standalone
-                            components, whereas Tailwind UI offers sections of pages.</p>
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both
-                            Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you
-                            from using the best of two worlds.</p>
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-                        <ul class="ps-5 text-gray-500 list-disc dark:text-gray-400">
-                            <li><a href="https://flowbite.com/pro/"
-                                    class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
-                            <li><a href="https://tailwindui.com/" rel="nofollow"
-                                    class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
-                        </ul>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">1. Formasi tenaga kerja ditetapkan oleh Pimpinan Perusahaan bersama-sama dengan penetapan Struktur Organisasi Perusahaan, uraian tugas (Job Description) tingkat golongan jabatan dan persyaratan-persyaratan dari tiaptiap jabatan/pekerja.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">2. Formasi tenaga kerja diperlukan untuk meningkatkan kinerja organisasi atau
+                            untuk menjaga produktivitas, efektifitas dan efisiensi perusahaan khususnya di
+                            dalam pengelolaan sumber daya manusia.
+                            </p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">3. Formasi tenaga kerja menjadi dasar utama untuk memungkinkan adanya :
+                            a. Penerimaan karyawan baru
+                            b. Penempatan dan pemindahan karyawan
+                            c. Pemberian promosi jabatan kepada karyawan.</p>
+                        <p class="mb-2 text-gray-500 dark:text-gray-400">4. Formasi tenaga kerja dapat sewaktu-waktu diubah oleh pimpinan perusahaan
+                            menurut keadaan dan kebutuhan / kepentingan perusahaan.</p>
+                        
                     </div>
                 </div>
                 <h2 id="accordion-collapse-heading-4">
